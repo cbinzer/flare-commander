0bf46cf9c7f00d8de8801cfd53b140d1f510fc57
Christian Binzer <cbinzer@users.noreply.github.com>
2025-02-13 22:18:44 +0100

    feat(kv): Add initial data table

---
+++ src/components/ui/data-table.tsx
@@ -0,0 +1,108 @@
+import {
+  Cell,
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  Header,
+  useReactTable,
+} from '@tanstack/react-table';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { useState } from 'react';
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [rowSelection, setRowSelection] = useState({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    onRowSelectionChange: setRowSelection,
+    getCoreRowModel: getCoreRowModel(),
+    state: {
+      rowSelection,
+    },
+  });
+
+  const getCustomWidth = (
+    item: Header<TData, unknown> | Cell<TData, unknown>,
+  ) => {
+    const meta = item.column.columnDef.meta as
+      | { width: string | undefined }
+      | undefined;
+    if (meta?.width) {
+      return meta.width;
+    }
+
+    return undefined;
+  };
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead
+                    key={header.id}
+                    style={{
+                      width: getCustomWidth(header),
+                    }}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && 'selected'}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell
+                    key={cell.id}
+                    style={{ width: getCustomWidth(cell) }}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
--- src/kv/kv-namespace-details.tsx
+++ src/kv/kv-namespace-details.tsx
@@ -8,6 +8,45 @@ import {
 } from '@/components/ui/breadcrumb.tsx';
 import { Location, useLocation, useNavigate } from 'react-router';
 import { KvNamespace } from '@/kv/kv-models.ts';
+import { ColumnDef } from '@tanstack/react-table';
+import { DataTable } from '@/components/ui/data-table.tsx';
+import { Checkbox } from '@/components/ui/checkbox';
+
+export const columns: ColumnDef<{ key: string; value: string }>[] = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && 'indeterminate')
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    meta: {
+      width: '30px',
+    },
+  },
+  {
+    id: 'key',
+    accessorKey: 'key',
+    header: 'Key',
+  },
+  {
+    id: 'value',
+    accessorKey: 'value',
+    header: 'Value',
+  },
+];
 
 const KvNamespaceDetails: FunctionComponent = () => {
   const navigate = useNavigate();
@@ -39,6 +78,18 @@ const KvNamespaceDetails: FunctionComponent = () => {
         <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
           {namespace.title}
         </h2>
+
+        <DataTable
+          columns={columns}
+          data={[
+            { key: 'key1', value: 'value1' },
+            { key: 'key2', value: 'value2' },
+            {
+              key: 'key3',
+              value: 'value3',
+            },
+          ]}
+        />
       </div>
     </>
   );
