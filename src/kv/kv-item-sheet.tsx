253a5ec807f5b06286cbce8faac359ba40ee0dd4
Christian Binzer <cbinzer@users.noreply.github.com>
2025-03-27 09:12:00 +0100

    feat(kv): Add skeleton elements to KvItemSheet

--- src/kv/kv-item-sheet.tsx
+++ src/kv/kv-item-sheet.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components/ui/button';
-import DateTimePicker from '@/components/ui/date-time-picker';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import {
@@ -12,10 +11,12 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet';
+import { Skeleton } from '@/components/ui/skeleton';
 import { Textarea } from '@/components/ui/textarea';
 import { FunctionComponent, useEffect, useRef, useState } from 'react';
 import { useKvItem } from './kv-hooks';
 import { KvItem } from './kv-models';
+import DateTimePicker from '@/components/ui/date-time-picker';
 
 export interface KvItemSheetProps {
   namespaceId: string;
@@ -26,7 +27,7 @@ const KvItemSheet: FunctionComponent<KvItemSheetProps> = ({ namespaceId, itemKey
   const { kvItem, loadKvItem } = useKvItem();
   const [sheetContainer, setSheetContainer] = useState<HTMLElement | null>(null);
 
-  const focusValueInputOnOpenChange = (open: boolean) => {
+  const loadKvItemOnOpenChange = (open: boolean) => {
     if (open) {
       loadKvItem(namespaceId, itemKey).then(() =>
         setSheetContainer(document.querySelector('[role="dialog"]') as HTMLElement),
@@ -35,7 +36,7 @@ const KvItemSheet: FunctionComponent<KvItemSheetProps> = ({ namespaceId, itemKey
   };
 
   return (
-    <Sheet onOpenChange={focusValueInputOnOpenChange}>
+    <Sheet onOpenChange={loadKvItemOnOpenChange}>
       <SheetTrigger asChild>
         <Button variant="link" className="w-fit px-0 text-left text-foreground">
           {itemKey}
@@ -76,27 +77,41 @@ const KvItemSheetContent: FunctionComponent<KvItemSheetContentProps> = ({ item, 
           <Label htmlFor="name" className="col-span-2 text-right">
             Name
           </Label>
-          <Input id="name" value={item?.key} className="col-span-10" disabled={true} />
+          {item ? (
+            <Input id="name" value={item?.key} className="col-span-10" disabled={true} />
+          ) : (
+            <Skeleton className="w-full h-8 rounded-md col-span-10" />
+          )}
         </div>
         <div className="grid grid-cols-12 items-start gap-4">
           <Label htmlFor="value" className="col-span-2 text-right pt-2">
             Value
           </Label>
-          <Textarea id="value" value={item?.value} className="col-span-10 min-h-[200px]" ref={valueInputRef} />
+          {item ? (
+            <Textarea id="value" value={item?.value} className="col-span-10 min-h-[200px]" ref={valueInputRef} />
+          ) : (
+            <Skeleton id="value" className="w-full h-48 rounded-md col-span-10" />
+          )}
         </div>
         <div className="grid grid-cols-12 items-center gap-4">
           <Label htmlFor="expiration" className="col-span-2 text-right">
             Expiration
           </Label>
-          <div className="col-span-10">
-            <DateTimePicker container={container} />
+          <div className="col-span-10 w-full">
+            {item ? (
+              <DateTimePicker container={container} value={item?.expiration} />
+            ) : (
+              <Skeleton className="w-full h-8 rounded-md" />
+            )}
           </div>
         </div>
       </div>
 
       <SheetFooter>
         <SheetClose asChild>
-          <Button type="submit">Save</Button>
+          <Button type="submit" disabled={!item}>
+            Save
+          </Button>
         </SheetClose>
       </SheetFooter>
     </SheetContent>
