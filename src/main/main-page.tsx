e4d2f688f13993f3ab0d356e6aae5045d24dc91b
Christian Binzer <cbinzer@users.noreply.github.com>
2025-04-11 22:55:30 +0200

    feat(app): Add logo to sidebar

--- src/main/main-page.tsx
+++ src/main/main-page.tsx
@@ -4,16 +4,36 @@ import {
   SidebarFooter,
   SidebarHeader,
   SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
   SidebarProvider,
 } from '@/components/ui/sidebar';
 import { KvSidebarGroup } from '@/kv/kv-sidebar-group.tsx';
 import { Outlet } from 'react-router';
+import { GalleryVerticalEnd } from 'lucide-react';
 
 export default function MainPage() {
   return (
     <SidebarProvider>
       <Sidebar collapsible="icon">
-        <SidebarHeader></SidebarHeader>
+        <SidebarHeader>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton size="lg" asChild>
+                <div>
+                  <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                    <GalleryVerticalEnd className="size-4" />
+                  </div>
+                  <div className="flex flex-col gap-0.5 leading-none">
+                    <span className="font-semibold">FlareCommander</span>
+                    <span className="">v0.1.0</span>
+                  </div>
+                </div>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
         <SidebarContent>
           <KvSidebarGroup />
         </SidebarContent>
