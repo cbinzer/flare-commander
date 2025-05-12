4128aa0e10b61e8a94880b860dafe618200e9d94
Christian Binzer <cbinzer@users.noreply.github.com>
2025-05-12 09:51:45 +0200

    feat(kv): Create a cloudflare holder for all services

--- src-tauri/src/cloudflare/mod.rs
+++ src-tauri/src/cloudflare/mod.rs
@@ -1,2 +1,39 @@
 pub mod account_details;
 pub mod read_key_value;
+
+pub struct Cloudflare {
+    api_url: String,
+    credentials: Credentials,
+    http_client: reqwest::Client,
+}
+
+impl Cloudflare {
+    pub fn new(credentials: Credentials, api_url: Option<String>) -> Self {
+        Self {
+            api_url: api_url.unwrap_or("https://api.cloudflare.com/client/v4".to_string()),
+            credentials,
+            http_client: reqwest::Client::new(),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum Credentials {
+    UserAuthKey { email: String, key: String },
+    UserAuthToken { token: String },
+    Service { key: String },
+}
+
+impl Credentials {
+    pub fn headers(&self) -> Vec<(&'static str, String)> {
+        match self {
+            Self::UserAuthKey { email, key } => {
+                vec![("X-Auth-Email", email.clone()), ("X-Auth-Key", key.clone())]
+            }
+            Self::UserAuthToken { token } => {
+                vec![("Authorization", format!("Bearer {}", token.clone()))]
+            }
+            Self::Service { key } => vec![("X-Auth-User-Service-Key", key.clone())],
+        }
+    }
+}
