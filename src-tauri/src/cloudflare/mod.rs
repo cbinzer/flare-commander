bfd5fc7e700f990cecd2a248ac9113186e283d85
Christian Binzer <cbinzer@users.noreply.github.com>
2025-05-12 10:32:52 +0200

    feat(kv): Create a new kv service

--- src-tauri/src/cloudflare/mod.rs
+++ src-tauri/src/cloudflare/mod.rs
@@ -1,39 +1,68 @@
+use std::sync::Arc;
+
 pub mod account_details;
+mod kv;
 pub mod read_key_value;
 
 pub struct Cloudflare {
     api_url: String,
     credentials: Credentials,
-    http_client: reqwest::Client,
+    http_client: Arc<reqwest::Client>,
+
+    pub kv: kv::Kv,
 }
 
 impl Cloudflare {
     pub fn new(credentials: Credentials, api_url: Option<String>) -> Self {
+        let api_url = api_url.unwrap_or("https://api.cloudflare.com/client/v4".to_string());
+        let http_client = Arc::new(reqwest::Client::new());
+
         Self {
-            api_url: api_url.unwrap_or("https://api.cloudflare.com/client/v4".to_string()),
-            credentials,
-            http_client: reqwest::Client::new(),
+            api_url: api_url.clone(),
+            credentials: credentials.clone(),
+            http_client: http_client.clone(),
+            kv: kv::Kv::new(credentials.clone(), Some(api_url), Some(http_client)),
         }
     }
 }
 
 #[derive(Clone, Debug)]
 pub enum Credentials {
-    UserAuthKey { email: String, key: String },
-    UserAuthToken { token: String },
-    Service { key: String },
+    UserAuthKey {
+        account_id: String,
+        email: String,
+        key: String,
+    },
+    UserAuthToken {
+        account_id: String,
+        token: String,
+    },
+    Service {
+        account_id: String,
+        key: String,
+    },
 }
 
 impl Credentials {
     pub fn headers(&self) -> Vec<(&'static str, String)> {
         match self {
-            Self::UserAuthKey { email, key } => {
+            Self::UserAuthKey {
+                account_id: _account_id,
+                email,
+                key,
+            } => {
                 vec![("X-Auth-Email", email.clone()), ("X-Auth-Key", key.clone())]
             }
-            Self::UserAuthToken { token } => {
+            Self::UserAuthToken {
+                account_id: _account_id,
+                token,
+            } => {
                 vec![("Authorization", format!("Bearer {}", token.clone()))]
             }
-            Self::Service { key } => vec![("X-Auth-User-Service-Key", key.clone())],
+            Self::Service {
+                account_id: _account_id,
+                key,
+            } => vec![("X-Auth-User-Service-Key", key.clone())],
         }
     }
 }
