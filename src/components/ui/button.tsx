d08787c82836fa73480524f1f8c623be95a40185
Christian Binzer <cbinzer@users.noreply.github.com>
2025-06-21 18:42:17 +0200

    feat(common): Update calendar and button components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,6 @@ const buttonVariants = cva(
         outline: 'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
-        blank: '',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
